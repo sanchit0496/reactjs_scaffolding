6464057c4b7fd6d388ac1f48e4f2c0e64683f15f
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-01-02 23:31:33 +0530

    adding react helmet to websie

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,7 +1,38 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 const About = () => {
-  return <div>About</div>
+  return (
+    <div>
+      <Helmet>
+        <title>About Us - MySite</title>
+        <meta
+          name="description"
+          content="Learn more about MySite, our mission, values, and the team behind our services."
+        />
+        <meta
+          name="keywords"
+          content="About MySite, MySite Team, MySite Mission, Our Services"
+        />
+        <link rel="canonical" href="http://mysite.com/about" />
+        <meta property="og:title" content="About Us - MySite" />
+        <meta
+          property="og:description"
+          content="Learn more about MySite, our mission, values, and the team behind our services."
+        />
+        <meta property="og:url" content="http://mysite.com/about" />
+        <meta property="og:type" content="website" />
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:title" content="About Us - MySite" />
+        <meta
+          property="twitter:description"
+          content="Learn more about MySite, our mission, values, and the team behind our services."
+        />
+        {/* Add more meta tags as needed */}
+      </Helmet>
+      About
+    </div>
+  )
 }
 
 export default About
