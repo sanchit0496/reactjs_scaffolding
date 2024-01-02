23dd68eef2df6f56c52a93c33e516c30255ea085
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2024-01-02 16:27:12 +0530

    added lazy load suspense and error boundary

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-// src/App.js
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import About from './pages/About/About'
 import Navbar from './components/Navbar/Navbar'
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary'
 import { SWRConfig } from 'swr'
+
+const Home = lazy(() => import('./pages/Home/Home'))
+const About = lazy(() => import('./pages/About/About'))
 
 const App = () => {
   return (
     <Router>
       <Navbar />
       <SWRConfig value={{ refreshInterval: 3000 }}>
-        <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <ErrorBoundary>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/about" element={<About />} />
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Suspense>
+        </ErrorBoundary>
       </SWRConfig>
     </Router>
   )
